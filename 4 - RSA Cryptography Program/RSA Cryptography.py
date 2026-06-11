6d2621497087e91136ab2d660be1333852b380c0
JaskaranM <159010231+JaskaranM@users.noreply.github.com>
2026-06-11 09:54:14 +0100

    Add files via upload

--- 4 - RSA Cryptography Program/RSA Cryptography.py
+++ 4 - RSA Cryptography Program/RSA Cryptography.py
@@ -2,7 +2,7 @@ import random
 import math
 from tkinter import *
 
-#checks if the numbers are prime
+
 def isPrime(number):
     if number < 2:
         return False
@@ -11,39 +11,35 @@ def isPrime(number):
             return False
     return True
 
-#generates prime number
 def randomPrimeNumber(minValue, maxValue):
     prime = random.randint(minValue, maxValue)
     while not isPrime(prime):
         prime = random.randint(minValue, maxValue)
     return prime
 
-#finds the modular inverse
 def inverseMod(e, phi):
     for d in range(3, phi):
         if (d * e) % phi == 1:
             return d
     raise ValueError("Can't find an inverse")
 
-#starts the tkinter window
 root = Tk()
 root.title("RSA Cryptography")
 root.configure(background="#D85336")
 root.minsize(750, 500)
 root.maxsize(750, 500)
 
-#centers everything
 root.grid_columnconfigure(0, weight=1)
 root.grid_columnconfigure(1, weight=1)
 root.grid_columnconfigure(2, weight=1)
 
-#starts all the lables and buttons
 label = Label(root, text="Enter Message: ")
 label.grid(column=0, row=1, padx=10, pady=10, sticky="e")
 entry = Entry(root, width=50)
 entry.grid(column=1, row=1, padx=10, pady=10, columnspan=2, sticky="w")
 resultLabel = Label(root, text="", bg="#D85336", fg="white", anchor="w", justify="left", wraplength=700)
 resultLabel.grid(column=0, row=4, padx=10, pady=10, columnspan=3, sticky="nsew")
+
 #contains the logic for encryption and decryption
 def RSAEncryption():
     message = entry.get()
@@ -63,18 +59,14 @@ def RSAEncryption():
     while math.gcd(e, phiN) != 1:
         e = random.randint(3, phiN - 1)
 
-    # calculates the private key
     d = inverseMod(e, phiN)
 
-    # encryption
     encryptedMessage = [ord(ch) for ch in message]
     cipherText = [pow(ch, e, n) for ch in encryptedMessage]
 
-    #decryption
     decryptedMessage = [pow(ch, d, n) for ch in cipherText]
     decryptedText = "".join(chr(ch) for ch in decryptedMessage)
 
-    #outputs results
     result = (f"Public key (e, n) = ({e}, {n})\n"
               f"Private key (d, n) = ({d}, {n})\n"
               f"Ciphertext: {cipherText}\n"
@@ -85,7 +77,7 @@ def RSAEncryption():
 
     resultLabel.config(text=result)
 
-#button to start RSA encryption
+
 button = Button(root, text="Encrypt/Decrypt", command=RSAEncryption)
 button.grid(column=0, row=2, padx=10, pady=10, columnspan=3, sticky="n")
 
